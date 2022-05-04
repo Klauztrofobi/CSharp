99d949429c9799459e0e68f7d82dff1b3a9c9d24
Klauztrofobi <97224194+Klauztrofobi@users.noreply.github.com>
2022-05-04 09:56:05 +0200

    Update Program.cs

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,58 +10,14 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-
-            ////Deklarere en variable av hver  av de nevte datatypene
-
-            //bool b;
-            //int i;
-            //float f;
-            //double d;
-            //string s;
-
-            ////tilordning
-
-            //b = true;
-            //i = 5;
-            //f = 3.2f;
-            //d = 1.2;
-            //s = "text haha kek";
-
-            //// Kan også Deklarere og tilornde samtidig
-
-            //bool b2 = false;
-            //int i2 = 3;
-            //float f2 = 1.3f;
-            //double d2 = 7.7;
-            //string s2 = "THK";
-
-            //var b3 = false;
-            //var i3 = 9;
-            //var f3 = 5.5f;
-            //var d3 = 9.9;
-            //var s3 = "text";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
             if (args.Length == 0)
             {
-                Console.WriteLine("Fuck Mannen ingen Parameteres her nå as.");
+                Console.WriteLine("ingen Parameteres her nå as.");
             }
             else
             {
-                Console.WriteLine("FUCK YOLO MANN: ");
+                Console.WriteLine("Tror parameter skal komme under her");
             }
             
             foreach (var covid in args)
