cfaf57f9dd5ca090732b14525be8d4b5f883e392
Klauztrofobi <97224194+Klauztrofobi@users.noreply.github.com>
2022-05-06 10:41:22 +0200

    Add files via upload

---
+++ Oppgaver Mellomuka/oppgave4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace oppgave4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            decimal b = 3.0m;
+            decimal sum = a + b;
+            Console.WriteLine(sum);
+        }
+    }
+}
