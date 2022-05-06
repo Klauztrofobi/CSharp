e69339d64a21f39d09f9d9b9ba5fecf1c638222c
Klauztrofobi <97224194+Klauztrofobi@users.noreply.github.com>
2022-05-06 10:40:02 +0200

    Add files via upload

---
+++ Oppgaver Mellomuka/oppgave3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace oppgave3
+{
+    internal class Program
+    {
+        static int tall1 = 5987;
+        static int tall2 = 75984;
+        static void Main(string[] args)
+        {
+            var toTall = TallSum(tall1, tall2);
+            Console.WriteLine(toTall);
+
+
+        }
+
+        static int TallSum(int tall1, int tall2)
+        {
+            return tall1 + tall2;
+        }
+        static void Ingenting()
+        {
+            return;
+        }
+    }
+}
