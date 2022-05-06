b0bed1cd0b39878e76040fdfdb83a64c9e7c4f5d
Klauztrofobi <97224194+Klauztrofobi@users.noreply.github.com>
2022-05-06 10:45:41 +0200

    Add files via upload

---
+++ Oppgaver Mellomuka/oppgave5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace oppgave5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, hva heter du?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Velkommen " + name);
+        }
+    }
+}
