fa885bc77231e723394f973144eb999c890f86ab
Klauztrofobi <97224194+Klauztrofobi@users.noreply.github.com>
2022-05-06 12:32:09 +0200

    Add files via upload

---
+++ C# Testing/Testing i C#/Converting.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    internal class Converting
+    {
+        static void something()
+        {
+            // to string
+            // anta varianle x - uansett datatype (av våre utvalgte)
+            var x = true;
+            string s = "" + x;
+            double d = 99.123;
+            string s2 = d.ToString("F");
+
+            // to int
+            int i = System.Convert.ToInt32(d);
+
+            float f = System.Convert.ToSingle(i);
+            float f2 = System.Convert.ToSingle(d);
+
+            double d2 = System.Convert.ToDouble(s2);
+
+
+        }
+    }
+}
---
+++ C# Testing/Testing i C#/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////Deklarere en variable av hver  av de nevte datatypene
+
+            //bool b;
+            //int i;
+            //float f;
+            //double d;
+            //string s;
+
+            ////tilordning
+
+            //b = true;
+            //i = 5;
+            //f = 3.2f;
+            //d = 1.2;
+            //s = "text haha kek";
+
+            //// Kan også Deklarere og tilornde samtidig
+
+            //bool b2 = false;
+            //int i2 = 3;
+            //float f2 = 1.3f;
+            //double d2 = 7.7;
+            //string s2 = "THK";
+
+            //var b3 = false;
+            //var i3 = 9;
+            //var f3 = 5.5f;
+            //var d3 = 9.9;
+            //var s3 = "text";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Fuck Mannen ingen Parameteres her nå as.");
+            }
+            else
+            {
+                Console.WriteLine("FUCK YOLO MANN: ");
+            }
+            
+            foreach (var covid in args)
+            {
+                Console.WriteLine("Parameter " + covid);
+            }
+        }
+    }
+}
